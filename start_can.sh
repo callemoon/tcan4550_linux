a370703ac8cb3bf3d589adec2d8a7b2d462490f8
calle <calle.moon@gmail.com>
2023-10-28 17:31:44 +0200

    queue size 1000

--- start_can.sh
+++ start_can.sh
@@ -5,5 +5,5 @@ sudo insmod tcan4550.ko
 
 sudo dtoverlay tcan4550-can0-overlay.dtbo
 
-sudo ip link set can0 up type can bitrate 500000
-sudo ifconfig can0 txqueuelen 350
+sudo ip link set can0 up type can bitrate 1000000
+sudo ifconfig can0 txqueuelen 1000
