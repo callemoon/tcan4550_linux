8a7ec95e1f02b1c5e9e3824b67c0823eb4e0ccae
Carl-Magnus Moon <calle.moon@gmail.com>
2023-11-11 13:24:56 +0100

    added restart-ms

--- start_can.sh
+++ start_can.sh
@@ -5,5 +5,5 @@ sudo insmod tcan4550.ko
 
 sudo dtoverlay tcan4550-can0-overlay.dtbo
 
-sudo ip link set can0 up type can bitrate 1000000
+sudo ip link set can0 up type can bitrate 1000000 restart-ms 1000
 sudo ifconfig can0 txqueuelen 1000
